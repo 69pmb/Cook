afe3a0d15813f804601bcafc8a4aa884ffcec562
pmb <pm.broca@gmail.com>
2020-01-31 21:50:07 +0100

    fix listing + links

--- src/main/java/utils/FilesUtils.java
+++ src/main/java/utils/FilesUtils.java
@@ -1,7 +1,10 @@
 package utils;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -13,15 +16,24 @@ public class FilesUtils {
      * @param folder     le dossier où chercher
      * @param files      la liste qui contiendra les résultats
      * @param extensions les extensions des fichiers à chercher
+     * @param exclude    TODO
      */
-    public static void listFilesForFolder(final File folder, List<File> files, List<String> extensions) {
-        for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                files.add(fileEntry);
-            } else if (extensions.stream()
-                    .anyMatch(extension -> StringUtils.endsWith(fileEntry.getName(), extension))) {
-                files.add(fileEntry);
+    public static void listFilesForFolder(final Path folder, List<Path> files, List<String> extensions,
+            String exclude) {
+        List<Path> collect;
+        try {
+            collect = Files.list(folder).collect(Collectors.toList());
+            for (final Path fileEntry : collect) {
+                if (Files.isDirectory(fileEntry)) {
+                    files.add(fileEntry);
+                } else if (extensions.stream()
+                        .anyMatch(extension -> StringUtils.endsWith(fileEntry.getFileName().toString(), extension))
+                        && !StringUtils.equalsIgnoreCase(fileEntry.getFileName().toString(), exclude)) {
+                    files.add(fileEntry);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
