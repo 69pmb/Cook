2ffb80d859c7afe1f539bb576f03c4906af371d0
pmb <pm.broca@gmail.com>
2020-01-31 21:50:07 +0100

    fix listing + links

--- src/main/java/cook/Cook.java
+++ src/main/java/cook/Cook.java
@@ -1,37 +1,58 @@
 package cook;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
+import org.apache.commons.lang3.StringUtils;
+
+import net.steppschuh.markdowngenerator.link.Link;
 import net.steppschuh.markdowngenerator.text.heading.Heading;
 import utils.FilesUtils;
 
 public class Cook {
 
     private static final List<String> EXTENSIONS = List.of("png", "jpg", "jpeg", "pdf");
+    // public static final String DIRECTORY = System.getProperty("user.dir");
+    public static final Function<Path, String> GET_FILE_NAME = file -> StringUtils
+            .substringBeforeLast(file.getFileName().toString(), ".");
+    public static final Comparator<Path> FILE_NAME_COMPARATOR = (c1, c2) -> String.CASE_INSENSITIVE_ORDER
+            .compare(GET_FILE_NAME.apply(c1), GET_FILE_NAME.apply(c2));
+    public static final String DIRECTORY = "C:\\Users\\Pierre-Marie\\Dropbox\\Cuisine";
+    public static final String RESULT_FILE = "Cook.md";
+    public static final String PREFIX_FILE = "file://";
 
-    /**
-     * @param args folder containing cooking recipe
-     */
-    public static void main(String[] args) {
-        if (args != null && args.length > 0) {
-            File root = new File(args[0]);
-            if (root.exists()) {
-                List<File> files = new ArrayList<>();
-                FilesUtils.listFilesForFolder(root, files, EXTENSIONS);
-                StringBuilder sb = new StringBuilder();
-                files.forEach(file -> {
-                    sb.append(new Heading(file.getName(), 1)).append("\n");
-                    if (file.isDirectory()) {
-                        List<File> children = new ArrayList<>();
-                        FilesUtils.listFilesForFolder(file, children, EXTENSIONS);
-                        children.forEach(child -> sb.append(new Heading(child.getName(), 4)).append("\n"));
-                    }
-                });
-                System.out.println(sb.toString());
-            }
+    public static void main(String[] args) throws Exception {
+        Path dir = Paths.get(DIRECTORY);
+        List<Path> files = new ArrayList<>();
+        FilesUtils.listFilesForFolder(dir, files, EXTENSIONS, RESULT_FILE);
+        if (files.isEmpty()) {
+            throw new Exception("Dossier: " + dir + " vide");
         }
+        StringBuilder sb = new StringBuilder();
+        files.stream().filter(Files::isDirectory).forEach(file -> {
+            sb.append(new Heading(file.getFileName().toString(), 1)).append("\n");
+            List<Path> children = new ArrayList<>();
+            FilesUtils.listFilesForFolder(file, children, EXTENSIONS, RESULT_FILE);
+            children.stream().sorted(FILE_NAME_COMPARATOR)
+            .forEach(child -> sb.append(
+                    new Link(GET_FILE_NAME.apply(child), PREFIX_FILE + child.toAbsolutePath().toString()))
+                    .append("\n"));
+        });
+        sb.append(new Heading("Divers", 1)).append("\n");
+        files.stream().filter(Predicate.not(Files::isDirectory)).sorted(FILE_NAME_COMPARATOR).forEach(f -> sb
+                .append(new Link(GET_FILE_NAME.apply(f), PREFIX_FILE + f.toAbsolutePath().toString())).append("\n"));
+        Files.write(Paths.get(DIRECTORY.concat(File.separator).concat(RESULT_FILE)),
+                sb.toString().getBytes(Charset.forName("UTF-8")), StandardOpenOption.CREATE);
     }
-
+    // [link](url){:target="_blank"}
+    // <a href="http://example.com/" target="_blank">example</a>
 }
