ff548b08c4e568242922b437e496292034792802
pmb <pm.broca@gmail.com>
2020-05-06 22:05:33 +0200

    build link page

--- src/main/java/cook/Cook.java
+++ src/main/java/cook/Cook.java
@@ -2,6 +2,7 @@ package cook;
 
 import java.util.List;
 
+import cook.domain.Child;
 import cook.domain.Parent;
 import utils.FilesUtils;
 import utils.MarkdownUtils;
@@ -13,16 +14,19 @@ public class Cook {
         if (parents.isEmpty()) {
             throw new Exception("Dossier vide");
         }
-        StringBuilder sb = MarkdownUtils.buildFileHeader();
+        MarkdownUtils mark = new MarkdownUtils();
+        mark.buildFileHeader(null);
+
         parents.stream().filter(p -> p.getFile().getIsDirectory()).forEach(parent -> {
-            sb.append(MarkdownUtils.buildHeadind(parent.getFile().getName()));
-            parent.getChildren().forEach(
-                    child -> sb.append(MarkdownUtils.buildLink(child.getFile(), parent.getFile().getFullName())));
+            mark.buildLinkPage(parent);
+            parent.getChildren().stream().filter(Child::isDirectory)
+            .forEach(child -> mark.buildLinkPage(parent, child));
         });
-        sb.append(MarkdownUtils
-                .buildHeadind((parents.stream().filter(p -> p.getFile().getIsDirectory()).count() + 1) + ". Divers"));
-        parents.stream().filter(p -> !p.getFile().getIsDirectory())
-        .forEach(parent -> sb.append(MarkdownUtils.buildLink(parent.getFile(), null)));
-        FilesUtils.writeFileResult(sb);
+
+        //        mark.buildLinkPage((parents.stream().filter(Parent::isDirectory).count() + 1) + ". Divers",
+        //                parents.stream().filter(Predicate.not(Parent::isDirectory)).count());
+        //        parents.stream().filter(Predicate.not(Parent::isDirectory))
+        //        .forEach(parent -> mark.buildLink(parent.getFile(), null));
+        FilesUtils.writeFileResult(mark.getSb());
     }
 }
--- src/main/java/utils/MarkdownUtils.java
+++ src/main/java/utils/MarkdownUtils.java
@@ -4,27 +4,45 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.Optional;
 
+import cook.domain.Child;
 import cook.domain.CookFile;
+import cook.domain.Parent;
 import net.steppschuh.markdowngenerator.link.Link;
 import net.steppschuh.markdowngenerator.text.heading.Heading;
 
 public class MarkdownUtils {
 
-    private static final String NEW_LINE = "  \n";
-    private static final String FILE_HEADER = "---\r\ntitle: Recette\r\n---";
+    private StringBuilder sb;
+    private final String NEW_LINE = "  \n";
+    private final String FILE_HEADER = "---\r\ntitle: %s\r\n---";
 
-    public static StringBuilder buildLink(CookFile file, String folder) {
-        return new StringBuilder().append(new Link(file.getName(), MarkdownUtils.buildPath(folder, file.getFullName())))
-                .append(" (").append(file.getSize()).append(" ").append(file.getExtension()).append(")")
-                .append(NEW_LINE);
+    public MarkdownUtils() {
+        sb = new StringBuilder();
     }
 
-    public static StringBuilder buildFileHeader() {
-        return new StringBuilder(FILE_HEADER + NEW_LINE);
+    public void buildLink(CookFile file, String folder) {
+        sb.append(new Link(file.getName(), MarkdownUtils.buildPath(folder, file.getFullName()))).append(" (")
+        .append(file.getSize()).append(" ").append(file.getExtension()).append(")").append(NEW_LINE);
     }
 
-    public static StringBuilder buildHeadind(String heading) {
-        return new StringBuilder().append(new Heading(heading, 1)).append(NEW_LINE);
+    public void buildFileHeader(String title) {
+        sb.append(String.format(FILE_HEADER, Optional.ofNullable(title).orElse("Recette"))).append(NEW_LINE);
+    }
+
+    private void buildHeadind(String heading, long count, int size) {
+        sb.append(new Heading(heading + " (" + count + ")", size)).append(NEW_LINE);
+    }
+
+    public void buildLinkPage(Parent parent) {
+        buildHeadind(new Link(parent.getFile().getName(), parent.getFile().getName() + ".md").toString(),
+                parent.countGrandChildren(), 1);
+    }
+
+    public void buildLinkPage(Parent parent, Child child) {
+        buildHeadind(
+                new Link(child.getFile().getName(), parent.getFile().getName() + ".md#" + child.getFile().getName())
+                .toString(),
+                child.getGrandChildren().size(), 2);
     }
 
     public static String buildPath(String folder, String file) {
@@ -32,15 +50,19 @@ public class MarkdownUtils {
                 + Optional.ofNullable(folder).map(dir -> folder + File.separator).orElse("") + Path.of(file);
     }
 
-    public static String buildArrowsHeader(String left, String right) {
+    public void buildArrowsHeader(String left, String right) {
         String[] array = FilesUtils.getArrows().toArray(new String[0]);
-        return "<p align=\"justify\">" + MarkdownUtils.buildImgLink(array[0], "Page précedente", left)
-        + MarkdownUtils.buildImgLink(array[1], "Page parente", "..")
-        + MarkdownUtils.buildImgLink(array[2], "Page suivante", right) + "</p>" + NEW_LINE;
+        sb.append("<p align=\"justify\">").append(MarkdownUtils.buildImgLink(array[0], "Page précedente", left))
+        .append(MarkdownUtils.buildImgLink(array[1], "Page parente", ".."))
+        .append(MarkdownUtils.buildImgLink(array[2], "Page suivante", right)).append("</p>").append(NEW_LINE);
     }
 
     public static String buildImgLink(String img, String tooltip, String url) {
         return "<a href=\"" + url + "\"><img src=\"" + img + "\" title=\"" + tooltip
                 + "\" style=\"height: 5vh\" /></a>";
+    }
+
+    public StringBuilder getSb() {
+        return sb;
     }
 }
